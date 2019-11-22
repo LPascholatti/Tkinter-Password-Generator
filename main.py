434ef4e121d1e826f84db5d5784ae2825e9974c3
LPascholatti <lucaspascholatti@gmail.com>
2019-11-22 12:35:52 +0100

    Fiel to include password length added

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import tkinter
 import random
 
-# string = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
-# passlen = 8
-# password =  "".join(random.sample(string,passlen))
-
 def generatePassword ():
   string = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
   passlen = 8
@@ -14,8 +10,10 @@ def generatePassword ():
 
 window = tkinter.Tk()
 # to rename the title of the window
-window.title("GUI")
+window.title("Password Generator")
 # pack is used to show the object in the window
-label = tkinter.Label(window, text = "Welcome to Lucas Pascholatti's Password Generator, below you will find your new password:").pack()
-label = tkinter.Label(window, text = generatePassword()).pack()
+introText = tkinter.Label(window, text = "Welcome to Lucas Pascholatti's Password Generator, below you will find your new password:").grid(row = 0)
+passwordLength = tkinter.Label(window, text = "Password Length.").grid(row = 1)
+lengthEntry = tkinter.Entry(window).grid(row = 1, column = 1)
+generatedPassword = tkinter.Label(window, text = "Your password is" + generatePassword()).grid(row = 2)
 window.mainloop()
