acc06d8041bea8691cc5e28dc583f5f7af44654c
LPascholatti <lucaspascholatti@gmail.com>
2019-11-22 12:44:23 +0100

    Add passlen in the function itself

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import tkinter
 import random
 
-def generatePassword ():
+def generatePassword (passlen):
   string = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
-  passlen = 8
   password =  "".join(random.sample(string,passlen))
   print(password)
   return password
@@ -13,7 +12,7 @@ window = tkinter.Tk()
 window.title("Password Generator")
 # pack is used to show the object in the window
 introText = tkinter.Label(window, text = "Welcome to Lucas Pascholatti's Password Generator, below you will find your new password:").grid(row = 0)
-passwordLength = tkinter.Label(window, text = "Password Length.").grid(row = 1)
-lengthEntry = tkinter.Entry(window).grid(row = 1, column = 1)
-generatedPassword = tkinter.Label(window, text = "Your password is" + generatePassword()).grid(row = 2)
+# passwordLength = tkinter.Label(window, text = "Password Length.").grid(row = 1)
+# lengthEntry = tkinter.Entry(window).grid(row = 1, column = 1)
+generatedPassword = tkinter.Label(window, text = "Your password is" + generatePassword(8)).grid(row = 1)
 window.mainloop()
