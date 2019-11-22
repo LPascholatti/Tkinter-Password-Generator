4601296cd429c72eeabbb8b9804edffa04531245
LPascholatti <lucaspascholatti@gmail.com>
2019-11-22 12:49:42 +0100

    Cleaned up the code from comments

--- main.py
+++ main.py
@@ -8,11 +8,8 @@ def generatePassword (passlen):
   return password
 
 window = tkinter.Tk()
-# to rename the title of the window
+
 window.title("Password Generator")
-# pack is used to show the object in the window
 introText = tkinter.Label(window, text = "Welcome to Lucas Pascholatti's Password Generator, below you will find your new password:").grid(row = 0)
-# passwordLength = tkinter.Label(window, text = "Password Length.").grid(row = 1)
-# lengthEntry = tkinter.Entry(window).grid(row = 1, column = 1)
 generatedPassword = tkinter.Label(window, text = "Your password is" + generatePassword(8)).grid(row = 1)
 window.mainloop()
