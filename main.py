b89083dcee14781998cb12364e83016ec0b15f36
LPascholatti <lucaspascholatti@gmail.com>
2019-11-22 12:26:43 +0100

    Create function to generate new password

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import tkinter
 import random
 
-string = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
-passlen = 8
-password =  "".join(random.sample(string,passlen))
+# string = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+# passlen = 8
+# password =  "".join(random.sample(string,passlen))
+
+def generatePassword ():
+  string = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+  passlen = 8
+  password =  "".join(random.sample(string,passlen))
+  print(password)
+  return password
 
 window = tkinter.Tk()
 # to rename the title of the window
 window.title("GUI")
 # pack is used to show the object in the window
 label = tkinter.Label(window, text = "Welcome to Lucas Pascholatti's Password Generator, below you will find your new password:").pack()
-label = tkinter.Label(window, text = password).pack()
+label = tkinter.Label(window, text = generatePassword()).pack()
 window.mainloop()
