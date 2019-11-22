85a80ef2339fa2eef8b0e3e0cd1b5c187bdfb27c
LPascholatti <lucaspascholatti@gmail.com>
2019-11-22 17:41:34 +0100

    Implement render new password based on input

--- main.py
+++ main.py
@@ -8,33 +8,31 @@ string = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&
 
 introText = Label(window, text = "Welcome to Lucas Pascholatti's Password Generator, below you will find your new password:")
 numberLength = Entry(window, width=20, borderwidth=3)
-# numberLength.insert(0, "Enter the password length")
 
-def generatePassword (passlen):
+def generatePassword(passlen):
   password =  "".join(random.sample(string,passlen))
-  print(password)
+  print("password:", password)
   return password
+
+def makePassword(passlen):
+  passlen = numberLength.get()
+  passlenInt = int(passlen)
+  newPassword = "Your new password is: " + generatePassword(passlenInt)
+  renderNewPassword = Label(window, text=newPassword)
+  renderNewPassword.grid(row = 6, column = 0)
+  print("newpassword:", newPassword)
 
 def getNumber():
   passlen = numberLength.get()
-  # generatedPassword = Label(window, text = "Your password is: " + generatePassword(number))
-  # generatedPassword.grid(row = 3, column=0)
-  print(passlen)
   renderNumber = Label(window, text=passlen)
   renderNumber.grid(row = 5, column = 0)
+  print("passlen in getNumber:", passlen)
   return passlen
 
 number = getNumber()
-print(number)
-
-def makePassword():
-  newPassword = generatePassword(number)
-  print(newPassword)
-  # renderNewPassword = Label(window, text="Your new password is: " + newPassword)
-  # renderNewPassword.grid(row = 7, column = 0)
+print("number", number)
 
 button = Button(window, text="Submit Password Length", padx=15, pady=15, fg="black", bg="white", command=getNumber)
-button2 = Button(window, text="Generate New Password", padx=15, pady=15, fg="black", bg="white", command=makePassword)
 
 generatedPassword = Label(window, text = "Your password is: " + generatePassword(8))
 generatedPassword.grid(row = 4, column=0)
@@ -42,6 +40,5 @@ generatedPassword.grid(row = 4, column=0)
 introText.grid(row = 0, column=0)
 numberLength.grid(row = 1, column=0)
 button.grid(row = 3, column=0)
-button2.grid(row = 6, column=0)
 
 window.mainloop()
