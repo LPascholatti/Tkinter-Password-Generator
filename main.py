ef910772f6b34572bb3c29844084887e8f0c6850
LPascholatti <lucaspascholatti@gmail.com>
2019-11-22 12:09:25 +0100

    Added password generator from lines 2 to 6

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import tkinter
+import random
+
+string = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+passlen = 8
+password =  "".join(random.sample(string,passlen))
 
 window = tkinter.Tk()
 # to rename the title of the window
 window.title("GUI")
 # pack is used to show the object in the window
-label = tkinter.Label(window, text = "Hello World!").pack()
+label = tkinter.Label(window, text = "Welcome to Lucas Pascholatti's Password Generator, below you will find your new password:").pack()
 window.mainloop()
